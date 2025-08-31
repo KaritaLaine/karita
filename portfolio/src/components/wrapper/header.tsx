7c89d867ad9d7a94f13e36b6a1a5e04f8ab35578
KaritaLaine <44746@edu.raseko.fi>
2025-08-31 19:45:06 +0300

    chore: add a comment

--- portfolio/src/components/wrapper/header.tsx
+++ portfolio/src/components/wrapper/header.tsx
@@ -5,6 +5,7 @@ import Settings from "../../assets/settings.png";
 export const Header = () => {
   return (
     <div>
+      {/* Navigation buttons */}
       <div className="flex gap-[3rem] w-full text-3xl font-semibold justify-end items-center pt-[3rem] pr-[5rem] flex-wrap">
         <a href="/"> About </a>
         <a href="/projects">Projects</a>
