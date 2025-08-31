c23f9aaaf2102810236dbf7d795e33ee190b87c1
KaritaLaine <44746@edu.raseko.fi>
2025-08-31 18:33:58 +0300

    feat: add a footer

---
+++ portfolio/src/components/wrapper/footer.tsx
@@ -0,0 +1,31 @@
+// TODO: add a this site was made with... text?
+
+export const Footer = () => {
+  return (
+    <div className="flex flex-col text-center font-light items-center gap-[0.3rem]">
+      <p>Designed & built by Karita</p>
+
+      {/* TODO: hide on projects page */}
+      <div className="italic flex gap-[0.2rem] text-sm">
+        About me icon made by
+        <a
+          href="https://www.flaticon.com/authors/bomsymbols"
+          title="BomSymbols"
+          className="text-accent"
+          target="_blank"
+        >
+          BomSymbols
+        </a>
+        from
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          className="text-accent"
+          target="_blank"
+        >
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+};
