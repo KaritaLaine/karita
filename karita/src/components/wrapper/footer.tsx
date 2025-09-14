1c375ee9dc72071d5eeb2753acdac647e5870d5e
KaritaLaine <44746@edu.raseko.fi>
2025-09-14 19:15:17 +0300

    feat: add tech higlights and links

--- karita/src/components/wrapper/footer.tsx
+++ karita/src/components/wrapper/footer.tsx
@@ -6,28 +6,46 @@ export const Footer = () => {
   return (
     <footer className="flex flex-col text-center font-light items-center h-[15rem] justify-center text-sm bg-cream">
       <p>Designed & developed by Karita</p>
-      <p>Built with React, TypeScript & Tailwind</p>
+      <p>
+        Built with{" "}
+        <span className="text-accent">
+          <a href="https://react.dev/" target="_blank">
+            React
+          </a>
+        </span>
+        ,{" "}
+        <span className="text-accent">
+          <a href="https://www.typescriptlang.org/" target="_blank">
+            TypeScript
+          </a>
+        </span>{" "}
+        &{" "}
+        <span className="text-accent">
+          <a href="https://tailwindcss.com/" target="_blank">
+            Tailwind
+          </a>
+        </span>
+      </p>
 
       {isHomePage && (
-        <div className="text-xs flex gap-[0.2rem] text-sm flex-wrap justify-center mt-[0.8rem]">
-          <p>About me icon made by</p>
-          <a
-            href="https://www.flaticon.com/authors/bomsymbols"
-            title="BomSymbols"
-            className="text-accent"
-            target="_blank"
-          >
-            BomSymbols
-          </a>
-          <p>from</p>
-          <a
-            href="https://www.flaticon.com/"
-            title="Flaticon"
-            className="text-accent"
-            target="_blank"
-          >
-            www.flaticon.com
-          </a>
+        <div className="text-xs flex flex-wrap justify-center mt-[0.8rem]">
+          <p>
+            About me icon made by{" "}
+            <span className="text-accent">
+              <a
+                href="https://www.flaticon.com/authors/bomsymbols"
+                target="_blank"
+              >
+                BomSymbols{" "}
+              </a>
+            </span>
+            from{" "}
+            <span className="text-accent">
+              <a href="https://www.flaticon.com/" target="_blank">
+                www.flaticon.com
+              </a>
+            </span>
+          </p>
         </div>
       )}
     </footer>
