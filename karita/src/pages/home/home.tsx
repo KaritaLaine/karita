215224218749014552bbad648c12afc3890feb59
KaritaLaine <44746@edu.raseko.fi>
2025-09-14 19:24:59 +0300

    feat: map through the technologies on home page

--- karita/src/pages/home/home.tsx
+++ karita/src/pages/home/home.tsx
@@ -3,6 +3,24 @@ import DownArrow from "../../assets/down-arrow.png";
 import { Wrapper } from "../../components/wrapper/wrapper";
 
 export const Home = () => {
+  const technologies = [
+    { name: "React", url: "https://react.dev/" },
+    { name: "React Native", url: "https://reactnative.dev/" },
+    { name: "Angular", url: "https://angular.dev/" },
+    {
+      name: "JavaScript",
+      url: "https://developer.mozilla.org/docs/Web/JavaScript",
+    },
+    { name: "TypeScript", url: "https://www.typescriptlang.org/" },
+    { name: "HTML", url: "https://developer.mozilla.org/docs/Web/HTML" },
+    { name: "CSS", url: "https://developer.mozilla.org/docs/Web/CSS" },
+    { name: "Chakra UI", url: "https://chakra-ui.com/" },
+    { name: "Tailwind", url: "https://tailwindcss.com/" },
+    { name: "Nativewind", url: "https://www.nativewind.dev/" },
+    { name: "Python", url: "https://www.python.org/" },
+    { name: "Django", url: "https://www.djangoproject.com/" },
+  ];
+
   return (
     <Wrapper>
       <div className="flex flex-col justify-center items-center">
@@ -17,7 +35,7 @@ export const Home = () => {
           <div className="flex items-center justify-center gap-[5rem]">
             <img src={AboutMeIcon} alt="about me icon" className="w-[18rem]" />
             <div className="w-[90%]">
-              <p className="text-4xl font-semibold mb-[2rem]"> About me </p>
+              <p className="text-5xl font-semibold mb-[2rem]"> About me </p>
 
               <p className="text-2xl">
                 I’m a third-year engineering student who enjoys building
@@ -43,87 +61,24 @@ export const Home = () => {
                 with technologies such as:
                 <br />
                 <br />
-                <span className="font-semibold text-accent">
-                  <a href="https://react.dev/" target="_blank">
-                    React
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://reactnative.dev/" target="_blank">
-                    React Native
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://angular.dev/" target="_blank">
-                    Angular
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a
-                    href="https://developer.mozilla.org/docs/Web/JavaScript"
-                    target="_blank"
-                  >
-                    JavaScript
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://www.typescriptlang.org/" target="_blank">
-                    TypeScript
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a
-                    href="https://developer.mozilla.org/docs/Web/HTML"
-                    target="_blank"
-                  >
-                    HTML
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a
-                    href="https://developer.mozilla.org/docs/Web/CSS"
-                    target="_blank"
-                  >
-                    CSS
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://chakra-ui.com/" target="_blank">
-                    Chakra UI
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://tailwindcss.com/" target="_blank">
-                    Tailwind
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://www.nativewind.dev/" target="_blank">
-                    Nativewind
-                  </a>
-                </span>
-                ,{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://www.python.org/" target="_blank">
-                    Python
-                  </a>
-                </span>{" "}
-                and{" "}
-                <span className="font-semibold text-accent">
-                  <a href="https://www.djangoproject.com/" target="_blank">
-                    Django
-                  </a>
-                </span>
-                .
+                {/* Listing out all technologies */}
+                {technologies.map((tech, i) => (
+                  <span key={tech.name}>
+                    <a
+                      href={tech.url}
+                      target="_blank"
+                      className="font-semibold text-accent"
+                    >
+                      {tech.name}
+                    </a>
+
+                    {i < technologies.length - 2
+                      ? ", "
+                      : i === technologies.length - 2
+                        ? " and "
+                        : "."}
+                  </span>
+                ))}
                 <br />
                 <br />
                 Don’t hesitate to reach out to me via{" "}
