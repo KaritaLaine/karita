5bf6c24b1adaa17ea1d2ff1bc41e2dc5b035b8a9
KaritaLaine <44746@edu.raseko.fi>
2025-09-14 16:19:55 +0300

    feat: edit header contents for projects page

--- karitala/src/components/wrapper/header.tsx
+++ karitala/src/components/wrapper/header.tsx
@@ -3,23 +3,17 @@ import { Link, useLocation } from "@tanstack/react-router";
 import Wave from "react-wavify";
 import Settings from "../../assets/settings.png";
 
-// TODO: highlight active page
 export const Header = () => {
   const location = useLocation();
+  const isHomePage = location.pathname === "/";
   return (
     <div>
       {/* Navigation buttons */}
       <div className="flex gap-[3rem] w-full text-3xl font-semibold justify-end items-center pt-[3rem] pr-[5rem] flex-wrap">
-        <Link
-          to="/"
-          className={`${location.pathname === "/" ? "text-accent" : ""}`}
-        >
+        <Link to="/" className={`${isHomePage ? "text-accent" : ""}`}>
           Home
         </Link>
-        <Link
-          to="/projects"
-          className={` ${location.pathname === "/projects" ? "text-accent" : ""}`}
-        >
+        <Link to="/projects" className={` ${!isHomePage ? "text-accent" : ""}`}>
           Projects
         </Link>
 
@@ -32,12 +26,20 @@ export const Header = () => {
         />
       </div>
 
-      <div className="flex justify-center items-center h-[50rem] flex-col gap-[1.5rem]">
-        <h1 className="text-[64px] font-bold text-center">Hi, I'm Karita</h1>
-        <h1 className="text-[40px] font-normal text-center">
-          a front-end developer & university student
-        </h1>
-      </div>
+      {isHomePage ? (
+        <div className="flex justify-center items-center h-[50rem] flex-col gap-[1.5rem]">
+          <h1 className="text-[4rem] font-bold text-center">Hi, I'm Karita</h1>
+          <h1 className="text-[2.5rem] font-normal text-center">
+            a front-end developer & university student
+          </h1>
+        </div>
+      ) : (
+        <div className="flex h-[20rem] justify-center items-center ">
+          <h1 className="text-[4rem] font-bold text-center mt-[6rem]">
+            Projects
+          </h1>
+        </div>
+      )}
 
       <Wave
         fill="#fff1e6"
@@ -45,8 +47,8 @@ export const Header = () => {
         style={{ display: "flex" }}
         options={{
           height: 30,
-          amplitude: 60,
-          speed: 0.2,
+          amplitude: 50,
+          speed: 0.15,
           points: 3,
         }}
       />
