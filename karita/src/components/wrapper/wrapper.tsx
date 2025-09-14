cbf9cdd4767ea300bdfe429304470d43c0cf7dd5
KaritaLaine <44746@edu.raseko.fi>
2025-09-14 19:15:26 +0300

    style: adjust spacing

--- karita/src/components/wrapper/wrapper.tsx
+++ karita/src/components/wrapper/wrapper.tsx
@@ -9,7 +9,7 @@ export const Wrapper = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <div className="flex align-center justify-center py-[3rem] bg-cream">
+      <div className="flex align-center justify-center py-[5rem] pb-[10rem] bg-cream">
         {children}
       </div>
       <Footer />
