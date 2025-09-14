eac1fe0b5511248e22e9435540df647010ebead3
KaritaLaine <44746@edu.raseko.fi>
2025-09-14 16:25:16 +0300

    style: adjust projects page spacing

--- karitala/src/pages/projects/projects.tsx
+++ karitala/src/pages/projects/projects.tsx
@@ -3,8 +3,10 @@ import { Wrapper } from "../../components/wrapper/wrapper";
 export const Projects = () => {
   return (
     <Wrapper>
-      <div>
-        <p> projects </p>
+      <div className="flex flex-col justify-center items-center">
+        <div className="mt-[8rem]">
+          <p> Projects </p>
+        </div>
       </div>
     </Wrapper>
   );
