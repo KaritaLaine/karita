3eecd910391cc594e937959e40cce1f8d6b260ae
KaritaLaine <44746@edu.raseko.fi>
2025-12-20 19:17:40 +0200

    refactor: use link text component in footer

--- karita/src/components/wrapper/footer.tsx
+++ karita/src/components/wrapper/footer.tsx
@@ -1,53 +1,30 @@
-import { useLocation } from "@tanstack/react-router";
+import { useLocation } from "@tanstack/react-router"
+import { LinkText } from "../linkText"
 
 export const Footer = () => {
-  const location = useLocation();
-  const isHomePage = location.pathname === "/";
+  const location = useLocation()
+  const isHomePage = location.pathname === "/"
   return (
-    <footer className="flex flex-col text-center font-light items-center h-[15rem] justify-center text-sm bg-cream">
+    <footer className="flex flex-col text-center font-light items-center h-[15rem] justify-center bg-cream">
       <p>Designed & developed by Karita</p>
       <p>
-        Built with{" "}
-        <span className="text-accent">
-          <a href="https://react.dev/" target="_blank">
-            React
-          </a>
-        </span>
-        ,{" "}
-        <span className="text-accent">
-          <a href="https://www.typescriptlang.org/" target="_blank">
-            TypeScript
-          </a>
-        </span>{" "}
-        &{" "}
-        <span className="text-accent">
-          <a href="https://tailwindcss.com/" target="_blank">
-            Tailwind
-          </a>
-        </span>
+        Built with <LinkText link="https://react.dev/" name="React" />,{" "}
+        <LinkText link="https://www.typescriptlang.org/" name="TypeScript" /> &{" "}
+        <LinkText link="https://tailwindcss.com/" name="Tailwind CSS" />
       </p>
 
       {isHomePage && (
-        <div className="text-xs flex flex-wrap justify-center mt-[0.8rem]">
+        <div className="text-sm flex flex-wrap justify-center mt-[1.2rem]">
           <p>
             About me icon made by{" "}
-            <span className="text-accent">
-              <a
-                href="https://www.flaticon.com/authors/bomsymbols"
-                target="_blank"
-              >
-                BomSymbols{" "}
-              </a>
-            </span>
-            from{" "}
-            <span className="text-accent">
-              <a href="https://www.flaticon.com/" target="_blank">
-                www.flaticon.com
-              </a>
-            </span>
+            <LinkText
+              link="https://www.flaticon.com/authors/bomsymbols"
+              name="BomSymbols"
+            />{" "}
+            from <LinkText link="https://www.flaticon.com/" name="Flaticon" />.
           </p>
         </div>
       )}
     </footer>
-  );
-};
+  )
+}
