b8cf1824cded623313d9445e0bcc08d0abb115a2
KaritaLaine <44746@edu.raseko.fi>
2025-12-20 20:01:03 +0200

    chore: run prettier

--- karita/src/routes/index.tsx
+++ karita/src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Home } from "../pages/home/home";
+import { createFileRoute } from "@tanstack/react-router"
+import { Home } from "../pages/home/home"
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
-  return <Home />;
+  return <Home />
 }
