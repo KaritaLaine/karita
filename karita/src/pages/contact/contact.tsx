e117fa79df35ef4c7ca42d52776f700a8cd6b94d
KaritaLaine <44746@edu.raseko.fi>
2025-12-20 19:18:10 +0200

    feat: add contact route

---
+++ karita/src/pages/contact/contact.tsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "../../components/wrapper/wrapper"
+
+export const Contact = () => {
+  return (
+    <Wrapper>
+      <div className="flex flex-col justify-center items-center">
+        <div className="mt-[8rem]">
+          <p> Contact </p>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
