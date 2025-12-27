bc27ef65dfb328a7df2aac42ec6b5b0a6ca5d1d9
KaritaLaine <44746@edu.raseko.fi>
2025-12-27 16:39:55 +0200

    refactor: adjust project descriptions

--- karita/src/data/myProjects.tsx
+++ karita/src/data/myProjects.tsx
@@ -1,35 +1,27 @@
 import nda from "../assets/nda.svg"
 import projectGate from "../assets/project-gate.svg"
 import restaurantFinder from "../assets/restaurant-finder.svg"
-import studyPals from "../assets/study-pals.svg"
 
 export const myProjects = [
   {
     name: "Project Gate",
     technologies: ["React", "Chakra UI", "TypeScript", "TanStack Router"],
     description:
-      "Worked on an internal theFIRMA project (2024-2025) creating a hub for software engineering students and teachers to manage study path related information and project applications.",
+      "Frontend developer on an internal theFIRMA project developing a website for students and teachers to manage study path information and project applications.",
     image: projectGate,
-  },
-  {
-    name: "Restaurant finder",
-    technologies: ["Figma"],
-    description:
-      "Designed a restaurant finder app as part of a UI & UX design course.",
-    image: restaurantFinder,
   },
   {
     name: "NDA project",
     technologies: ["React Native", "Nativewind", "Expo", "TypeScript"],
     description:
-      "Took part in an NDA customer project developing a responsive mobile application for Android and iOS.",
+      "Frontend developer on a team that built a responsive mobile app prototype for a customer project on Android and iOS.",
     image: nda,
   },
   {
-    name: "Study pals",
-    technologies: ["Django", "JavaScript", "HTML", "CSS"],
+    name: "Restaurant finder",
+    technologies: ["Figma"],
     description:
-      "Took part in designing and building a website prototype where students can find and connect with others to study together.",
-    image: studyPals,
+      "Designed a restaurant finder application for a UI/UX design course.",
+    image: restaurantFinder,
   },
 ]
