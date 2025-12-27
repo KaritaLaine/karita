974a17a93e10889fcb0ca2cc8b8d37b13162d54b
KaritaLaine <44746@edu.raseko.fi>
2025-12-27 16:40:57 +0200

    style: adjust spacing and font styling

--- karita/src/pages/projects/components/myProjects.tsx
+++ karita/src/pages/projects/components/myProjects.tsx
@@ -8,27 +8,29 @@ export const MyProjects = () => {
       {projects.map((project, index) => (
         <div
           key={index}
-          className="flex flex-col justify-center items-center md:items-start gap-[2rem] md:flex-row md:gap-[6%] p-[2rem] rounded-xl w-full"
+          className="flex flex-col justify-center items-center md:items-start gap-[1.5rem] md:flex-row md:gap-[6%] p-[2rem] rounded-xl w-full"
         >
-          <h1 className="text-2xl md:hidden font-semibold">{project.name}</h1>
+          <h1 className="text-3xl md:hidden font-semibold">{project.name}</h1>
           <img
             src={project.image}
             alt={project.name}
-            className="max-w-md rounded-xl border-2 border-[#D9D9D9] shadow-md flex-shrink-0 lg:w-full md:w-[50%] w-full"
+            className="max-w-md rounded-xl border-[0.5px] border-[#D9D9D9] shadow-md flex-shrink-0 lg:w-full md:w-[50%] w-full"
           />
 
-          <div className="flex flex-col gap-[1rem] w-full max-w-md md:text-lg md:max-w-lg">
-            <h1 className="text-2xl hidden md:flex font-semibold">
+          <div className="flex flex-col gap-[1.2rem] w-full max-w-md">
+            <h1 className="text-3xl hidden md:flex font-semibold">
               {project.name}
             </h1>
 
-            <div className="flex flex-wrap justify-center md:justify-start bg-[#D9D9D980] gap-2 p-[0.5rem] rounded-xl w-full">
-              <p className="text-sm font-medium text-center md:text-left px-[1rem]">
+            <div className="md:text-base text-sm md:justify-start bg-[#D9D9D980] gap-2 p-[0.4rem] rounded-xl w-full">
+              <p className="mx-[0.8rem] font-medium text-center md:text-left">
                 {project.technologies.join(", ")}
               </p>
             </div>
 
-            <p className="text-justify md:text-left">{project.description}</p>
+            <p className="text-lg text-justify md:text-left text-[#453939]">
+              {project.description}
+            </p>
           </div>
         </div>
       ))}
